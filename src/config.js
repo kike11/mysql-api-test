77e2d7863c0a2ba00ca1aaebdbe2971d8c932a60
Enrique ER. Rodriguez <erodriguez@grupomar.com>
2023-03-27 17:19:30 -0600

    segunada coneccion eliminada

--- src/config.js
+++ src/config.js
@@ -11,10 +11,10 @@ export const config = {
  
   }
 
-  export const config2 = {
-  host: process.env.DB_HOST2,
-  user: process.env.DB_USER2,
-  password: process.env.DB_PASSWORD2,
-  database: process.env.DB_NAME2,
+  // export const config2 = {
+  // host: process.env.DB_HOST2,
+  // user: process.env.DB_USER2,
+  // password: process.env.DB_PASSWORD2,
+  // database: process.env.DB_NAME2,
   
-  }
+  // }
